e628c54cb2d19989878a42e0f496b955e94e700a
Daire O'Neill <daireon008@gmail.com>
2023-04-05 18:11:08 +0000

    End of file and completed file is now displayed

--- receiver_assembler.c
+++ receiver_assembler.c
@@ -6,6 +6,10 @@
 
 struct file_node{
 int ID;
+int complete;
+int num_data_nodes;
+int end_present;
+int lastID;
 struct data_node * data;
 struct file_node *left;
 struct file_node *right;
@@ -19,7 +23,7 @@ struct data_node *right;
 };
 
 void data_insert_order(struct data_node * root);struct data_node *get_data_node(int value,char *word);
-struct data_node * data_insert(struct data_node * root,int ID,char * message);
+struct data_node * data_insert(struct data_node * root,int ID,char * message,struct file_node * f);
 struct file_node *getNode(int value);
 struct file_node *insert(struct file_node *root, int value);
 void insertorder(struct file_node *root);
@@ -30,6 +34,7 @@ void create_file(struct file_node * root, int value);
 void add_to_file(FILE * f, struct data_node * root);
 char *search_seg(struct file_node * root, int fileID, int segID);
 struct data_node * find_data_node(temp,segID);
+void complete_files(struct file_node * root);
 
 struct data_node *get_data_node(int value,char *word){
 struct data_node *newNode = malloc(sizeof(struct data_node));
@@ -37,22 +42,22 @@ newNode->left = NULL;
 newNode->right = NULL;
 newNode->ID = value;
 int wordlength = strlen(word);
-//char * temp = (char*)malloc((wordlength+1)*sizeof(char));///////////////////////////////////
 char * temp = (char*)malloc((MX_STR_LEN)*sizeof(char));
  strcpy(temp,word);
 newNode->message = temp;
 return newNode;
 }
 
-struct data_node * data_insert(struct data_node * root,int ID,char * message){
+struct data_node * data_insert(struct data_node * root,int ID,char * message, struct file_node * f){
 if(root == NULL){
+    f->num_data_nodes++;//adds to the number of data nodes per file node
 return get_data_node(ID,message);
 }
 if(root->ID < ID){
-root->right = data_insert(root->right,ID,message);
+root->right = data_insert(root->right,ID,message,f);
 }
 else if(root->ID > ID){
-root->left = data_insert(root->left,ID,message);
+root->left = data_insert(root->left,ID,message,f);
 }
 return root; 
 }
@@ -63,6 +68,10 @@ newNode->left = NULL;
 newNode->right = NULL;
 newNode->data = NULL;
 newNode->ID = value;
+newNode->complete = 0;
+newNode->num_data_nodes = 0;
+newNode->end_present = 0;
+newNode->lastID = 0;
 return newNode;
 }
 
@@ -80,8 +89,9 @@ return root;
 }
 
 void insertorder(struct file_node *root){
-if(root == NULL)
-return;
+if(root == NULL){
+    return;
+}
 insertorder(root->left);
 printf("%d ",root->ID);
 insertorder(root->right);
@@ -113,7 +123,7 @@ struct file_node * find_node(struct file_node * root, int value){
 }
 
 struct file_node *parse_message(struct file_node *root,char*dataseg){
-    int id, seg, wordlength;
+    int id, seg, wordlength,end;
     char* word;
 
     // parse msg
@@ -126,6 +136,8 @@ struct file_node *parse_message(struct file_node *root,char*dataseg){
     // read in seg ID
     parse = strtok(NULL, "|");
     seg = atoi(parse);
+    parse = strtok(NULL, "|");
+    end = atoi(parse);
     // read in data
     parse = strtok(NULL, "\0");
     wordlength = strlen(parse);
@@ -140,10 +152,16 @@ struct file_node *parse_message(struct file_node *root,char*dataseg){
     c = find_node(root,id);
     
     //inserting seg in file node binary tree
-    c->data = data_insert(c->data,seg,word);
-    //
-    //printf("the message in c->data is %s\n",c->data->message);
-    ///
+    c->data = data_insert(c->data,seg,word,c);
+    //if the last segment in a file is present, mark that on the node
+    if(end == 1){
+        c->end_present = 1;
+        c->lastID = seg;
+    }
+    //check to see if the file is complete
+    if(c->lastID != 0 && (c->lastID - 99)==c->num_data_nodes){
+        c->complete = 1;
+    }
     return root;
 }
 
@@ -218,29 +236,40 @@ char *search_seg(struct file_node * root, int fileID, int segID){
     return NULL;
 }
 
+void complete_files(struct file_node * root){
+    if(root == NULL){
+        return;
+    }
+    complete_files(root->left);
+    if(root->complete == 1){
+        printf("%d ",root->ID);
+    }
+    complete_files(root->right);
+}
+
 int main(){
 struct file_node *root = NULL;
 
-root = parse_message(root,"198.10.12.145|0|1111|13|100|hjksdlhgvshgrsvhnkcshdgvhniskrdgc\n");
-root = parse_message(root,"198.10.12.145|0|1111|15|109|babbabbbabababadbflasdjfbsaf\n");
-root = parse_message(root,"198.10.12.145|0|1111|13|105|shingodingobingo\n");
-root = parse_message(root,"198.10.12.145|0|1111|15|103|aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
-root = parse_message(root,"198.10.12.145|0|1111|222|107|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|13|108|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|15|102|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|13|101|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|15|100|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|13|100|0|hjksdlhgvshgrsvhnkcshdgvhniskrdgc\n");
+root = parse_message(root,"198.10.12.145|0|1111|15|109|1|babbabbbabababadbflasdjfbsaf\n");
+root = parse_message(root,"198.10.12.145|0|1111|13|101|0|shingodingobingo\n");
+root = parse_message(root,"198.10.12.145|0|1111|15|103|0|aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
+root = parse_message(root,"198.10.12.145|0|1111|222|107|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|13|103|1|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|15|102|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|13|102|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|15|100|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
 
-root = parse_message(root,"198.10.12.145|0|1111|2222|109|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|108|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|107|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|106|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|105|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|104|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|103|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|102|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|101|1234567891011121314151617181920212223242526272829303132333435363738\n");
-root = parse_message(root,"198.10.12.145|0|1111|2222|100|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|109|1|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|108|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|107|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|106|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|105|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|104|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|103|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|102|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|101|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
+root = parse_message(root,"198.10.12.145|0|1111|2222|100|0|1234567891011121314151617181920212223242526272829303132333435363738\n");
 
 
 //TODO:
@@ -274,6 +303,12 @@ temp = search_seg(root,15,103);
 printf("char returned is %s\n",temp);
 temp = search_seg(root,2222,100);
 printf("char returned is %s\n",temp);
+
+//root = root->right;
+//root = root->right;
+//root = root->right;
+printf("the number of node is %d and the last id is %d and complete status is %d\n",root->num_data_nodes,root->lastID,root->complete);
+complete_files(root);
 
 return 0;
 }
